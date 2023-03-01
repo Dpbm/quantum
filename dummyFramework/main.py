6105c8504991b8e64f6eb337b9c509607b44a37e
Dpbm <dpbm136@gmail.com>
2023-03-01 15:42:23 -0300

    testing HZH circuit

--- dummyFramework/main.py
+++ dummyFramework/main.py
@@ -1,14 +1,11 @@
 from quantum.circuit import Circuit
-from quantum.gates import X, H
+from quantum.gates import X, H, Z
 
-circuit = Circuit(total_of_qubits=2)
-circuit.add_single_qubit_gate(X, 0)
+circuit = Circuit(total_of_qubits=1)
 circuit.add_single_qubit_gate(H, 0)
-#circuit.add_single_qubit_gate(X, 0)
-circuit.add_single_qubit_gate(H, 0)
+circuit.add_single_qubit_gate(Z, 0)
 
 
-circuit.add_single_qubit_gate(X, 1)
 
 circuit.show_circuit()
 circuit.show_states()
