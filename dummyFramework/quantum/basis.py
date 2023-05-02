01b9e8c96a07d220c0f333f9f03341063c1449c0
Dpbm <dpbm136@gmail.com>
2023-05-01 21:04:25 -0300

    added more basis

--- dummyFramework/quantum/basis.py
+++ dummyFramework/quantum/basis.py
@@ -21,3 +21,23 @@ class One(Basis):
     def __init__(self):
         self.basis_vector = operations.create_vector([[0], [1]])
         self.basis_symbol = '|1⟩'
+
+class Plus(Basis):
+    def __init__(self):
+        self.basis_vector = operations.create_vector([[1/operations.square_root(2)], [1/operations.square_root(2)]])
+        self.basis_symbol = '|+⟩'
+
+class Minus(Basis):
+    def __init__(self):
+        self.basis_vector = operations.create_vector([[1/operations.square_root(2)], [-1/operations.square_root(2)]])
+        self.basis_symbol = '|-⟩'
+
+class Plus_i(Basis):
+    def __init__(self):
+        self.basis_vector = operations.create_vector([[0], [1j]])
+        self.basis_symbol = '|i⟩'
+
+class Minus_i(Basis):
+    def __init__(self):
+        self.basis_vector = operations.create_vector([[-1j], [0]])
+        self.basis_symbol = '|-i⟩'
